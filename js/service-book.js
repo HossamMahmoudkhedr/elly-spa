dea41a6223065c51feeebc91ca1cdee50db60c45
HossamMahmoudkhedr <nhem318@gmail.com>
2024-08-14 18:38:18 +0300

    add checkout page

--- js/service-book.js
+++ js/service-book.js
@@ -20,31 +20,13 @@ const serviceName = document.getElementById('product-name');
 const servicePrice = document.getElementById('product-price');
 const serviceDescription = document.getElementById('product-description');
 const serviceNumberOfRates = document.getElementById('number-of-rates');
-const numberOfOrders = document.getElementById('number-of-orders');
-const countUpElement = document.getElementById('count-up');
-const countDownElement = document.getElementById('count-down');
+
 const serviceNameContainer = document.getElementById('service-name');
 const info = document.getElementById('benefits');
 const ServiceTypesContainer = document.querySelector('.service-types');
 const duration = document.getElementById('duration');
 
 const serviceImages = currentServiceType.serviceImages;
-let counter = 1;
-const countUp = () => {
-	counter++;
-	setData(numberOfOrders, counter);
-};
-const countDown = () => {
-	if (counter > 0) {
-		counter--;
-		setData(numberOfOrders, counter);
-	}
-	if (counter == 0) {
-		setData(numberOfOrders, '0');
-	}
-};
-
-setData(numberOfOrders, counter);
 
 createMainImg(serviceImages);
 createSubImgs(serviceImages);
@@ -55,8 +37,6 @@ setData(serviceDescription, currentServiceType.description);
 setData(serviceNumberOfRates, currentServiceType.numberOfRates);
 setData(info, currentServiceType.info);
 setData(duration, currentServiceType.duration);
-countUpElement.addEventListener('click', countUp);
-countDownElement.addEventListener('click', countDown);
 
 let types = currentService.types.filter(
 	(service) => service.id != serviceTypeId
